2f39d3b63a7ec1317e290e1e92783bf88f6a260b
arashpreet singh <sing1471@algonquilve.com>
2021-12-10 14:05:15 -0300

    inprogress

---
+++ script.js
@@ -0,0 +1,8 @@
+let hamburger = document.querySelector(".box");
+
+let navlink = document.querySelector("ul > a");
+
+
+hamburger.addEventListener("click",function() {
+    hamburger.classList.toggle("is-active");
+  })
